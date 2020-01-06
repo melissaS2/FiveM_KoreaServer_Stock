816d14d99ca1f17a15cf6d099a666f06bfcb3e4a
melissaS2 <good4uprin@gmail.com>
2020-01-06 11:01:13 +0900

    first

---
+++ OutSourcing/AttendanceMgmt/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttendanceMgmt.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/ 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public ActionResult Welcome(string name, int numTimes)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+
+            //return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
